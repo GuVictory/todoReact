4c733ee1530cdf08d6e6797a787c746a31a4c017
GuVictory <gubanova.vi10@gmail.com>
2020-08-02 22:33:34 +0500

    first comps

---
+++ src/components/search-panel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchPanel = () => {
+	const searchStyle = {
+		fontSize: '1.5em',
+	};
+	return <input placeholder="search" style={searchStyle} />;
+};
+
+export default SearchPanel;
